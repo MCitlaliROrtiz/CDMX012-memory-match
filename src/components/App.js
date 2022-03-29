da5b6f04315446047d1bb921fda013e95915a113
MCitlaliROrtiz <marciror@gmail.com>
2022-03-29 13:18:04 -0600

    this commit contains elements and node creations

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,32 @@
 import data from '../data/webdev/webdev.js';
 import { shuffle } from '../logic.js';
 
-const webData=data.items
+  const App = () => {
+
+    const el = document.createElement('section');
+    const webData=data.items
+
 const duplicateData=webData.concat(webData);
 
 let random=shuffle(duplicateData);
   
+ //crear un nodo por cada item
+function createNode(item) {
+  let card=document.createElement("section");
+  //console.log(item);
+  card.className="cardStyle"
+   let image=document.createElement("img");
+   image.src=item.image;
+   card.appendChild(image);
+   el.appendChild(card);
+}
+
+ random.forEach(createNode);
+
  
-const App = () => {
-  const el = document.createElement('section');
 
   el.className = 'App';
-  el.textContent = '';
+  
 
   return el;
 };
