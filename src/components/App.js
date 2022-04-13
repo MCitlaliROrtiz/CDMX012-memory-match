a5c6152bbd04652b6858037c85d9434df65319a2
MCitlaliROrtiz <marciror@gmail.com>
2022-04-13 09:53:47 -0500

    background image

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,51 @@ import { shuffle } from '../logic.js';
 
     const el = document.createElement('section');
     const webData=data.items
-
 const duplicateData=webData.concat(webData);
-
+let counter=0;
 let random=shuffle(duplicateData);
+
+/*function rotateCard() {
+  console.log("funciona?");
+}*/
   
  //crear un nodo por cada item
 function createNode(item) {
+  let container=document.createElement("section");
+  container.className="container";
+  
+  //creacion de nodo card y sus elementos
   let card=document.createElement("section");
-  //console.log(item);
-  card.className="cardStyle"
+   card.style.backgroundColor=item.bgColor;
+   //console.log(card.style);
+   card.className="cardStyle"
    let image=document.createElement("img");
    image.src=item.image;
+   let name=document.createElement("p");
+   name.textContent=item.id;
+   card.appendChild(name);
    card.appendChild(image);
-   el.appendChild(card);
+   //creacion del nodo reverse y sus elementos
+   let reverse=document.createElement("section");
+   reverse.className="cardReverse";
+   //insercion de nodos card y reverse en nodo el
+   container.appendChild(card);
+   container.appendChild(reverse);
+    //evento a para girar tarjetas
+   container.addEventListener("click",function() {
+     counter++;
+     if(counter===1) {
+      reverse.style.visibility="hidden";
+      let firstCard=item.id;
+     } 
+    if(counter===2){
+      reverse.style.visibility="hidden";
+      let secondCard=item.id;
+     }
+   if(counter>2){
+    reverse.style.visibility="visible";}
+  })
+   el.appendChild(container);
 }
 
  random.forEach(createNode);
--- src/data/webdev/webdev.js
+++ src/data/webdev/webdev.js
@@ -5,11 +5,11 @@ export default {
     { id: 'git', image: 'https://simpleicons.org/icons/git.svg', bgColor: '#F05032' },
     { id: 'css', image: 'https://simpleicons.org/icons/css3.svg', bgColor: '#1572B6' },
     { id: 'html', image: 'https://simpleicons.org/icons/html5.svg', bgColor: '#E34F26' },
-    { id: 'node', image: 'https://simpleicons.org/icons/node-dot-js.svg', bgColor: '#339933' },
+    { id: 'node', image: 'https://cdn.imgbin.com/11/14/8/imgbin-node-js-javascript-express-js-angularjs-random-icons-7WDWYsuHtir9S25rzrrtFBh9m.jpg', bgColor: '#339933' },
     { id: 'npm', image: 'https://simpleicons.org/icons/npm.svg', bgColor: '#CB3837' },
     { id: 'yarn', image: 'https://simpleicons.org/icons/yarn.svg', bgColor: '#2C8EBB' },
     { id: 'react', image: 'https://simpleicons.org/icons/react.svg', bgColor: '#61DAFB' },
     { id: 'angular', image: 'https://simpleicons.org/icons/angular.svg', bgColor: '#DD0031' },
-    { id: 'vue', image: 'https://simpleicons.org/icons/vue-dot-js.svg', bgColor: '#4FC08D' },
+    { id: 'vue', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNObPmF46odHwhlZgwdfH18nfS5fnODOgK8k3msq4Tx7QLIFVPaFIuGMJXUY7Ntlve8HE&usqp=CAU', bgColor: '#4FC08D' },
   ],
 };
