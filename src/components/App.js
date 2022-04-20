be1ca12df1269583b467007894dcbee608d622c0
MCitlaliROrtiz <marciror@gmail.com>
2022-04-19 23:32:51 -0500

    this commit contains the final version of the game funcionality

--- src/components/App.js
+++ src/components/App.js
@@ -12,15 +12,14 @@ let score=0;
 let firstCard=null;
 let secondCard=null;
 let random=shuffle(duplicateData);
+let styleFirstCard=null;
 console.log();
-/*function rotateCard() {
-  console.log("funciona?");
-}*/
+
   
  //crear un nodo por cada item
 function createNode(item) {
-  let container=document.createElement("section");
-  container.className="container";
+  let cardContainer=document.createElement("section");
+  cardContainer.className="cardContainer";
   
   //creacion de nodo card y sus elementos
   let card=document.createElement("section");
@@ -37,10 +36,11 @@ function createNode(item) {
    let reverse=document.createElement("section");
    reverse.className="cardReverse";
    //insercion de nodos card y reverse en nodo el
-   container.appendChild(card);
-   container.appendChild(reverse);
+   cardContainer.appendChild(card);
+   cardContainer.appendChild(reverse);
     //evento a para girar tarjetas
-   container.addEventListener("click",function() {
+    
+   cardContainer.addEventListener("click",function() {
      /* necesito que  solo se puedan dar 2 clicks por turno
      que se guarde el item.id en una variable por cada click
      luego comparar si son iguales(match)
@@ -50,40 +50,58 @@ function createNode(item) {
      crear  una variable score que aumente uno cada vez que el match ocurra y 
      cuando score sea igual a 10   mostrar un aviso al jugador de que ha ganado
      tener en cuenta limpiar las variables de second y first card(igual que counter) */
-     counter++;
-    if(counter===1&&counter===2) {
-     
-      firstCard=item.id;
-      secondCard=item.id;
-     if(firstCard===secondCard){
-       score++
-       reverse.style.visibility="hidden";
-     }
-     else{
-      reverse.style.visibility="visible";
-     }
-                                                          
-     } 
+    //empezar a contar clicks
+    counter++;
+    //primer click
+    if(counter===1) {
+     reverse.style.visibility="hidden";
+     firstCard=item.id;
+     styleFirstCard=reverse;
+     console.log(styleFirstCard);
     
-      
-    /*  ;
-     }){
+     } //segundo click
+     else if (counter===2) {
+       reverse.style.visibility="hidden";
+      secondCard=item.id;
+      console.log(firstCard);
+   
+        //match de id
+      if(firstCard===secondCard){
+         score++
+         console.log("score",score);
+        }
+      //id no hacen match
+      if(firstCard!==secondCard){
+        reverse.style.visibility="hidden";
+        styleFirstCard.style="visibility:hidden";
+        setTimeout(()=>{reverse.style.visibility="visible"},2000);
+        setTimeout(()=>{styleFirstCard.style.visibility="visible"},2000);
+        }
+        counter=0;
+        if(score===10){
+          console.log("si se ejecuta");
+          let oldNodo=el.removeChild(cardContainer);
+          console.log(oldNodo);
+          let congratulatios=document.createElement("section");
+          congratulatios.className="winner";
+          let mensage=document.createElement("p");
+          mensage.textContent="congratulations you win!!";
+          congratulatios.appendChild(mensage);
+          el.appendChild(congratulatios);
+        }
+      }
+    
      
-       score++;
-       console.log("sirve");
-
-     }e
+       
+   
       
-     }
-   else if(counter>2){
-    reverse.style.visibility="visible";}
-  })*/
-  
-   el.appendChild(container);
+   
+   });
+
+   el.appendChild(cardContainer)
 }
 
  random.forEach(createNode);
-
  
 
   el.className = 'App';
